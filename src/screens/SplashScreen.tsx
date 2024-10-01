f9ff8be163afa8d07c449c6f54194f9257669c83
augustine-awuori <codewithaugustine@gmail.com>
2024-10-01 11:10:17 +0300

    Change file structure

---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+
+export default () => {
+  return <View style={styles.container}></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {},
+});
