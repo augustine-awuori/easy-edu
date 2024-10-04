8a7e08fd235f1d7cc6e3c7c937f90c02e1882d86
augustine-awuori <codewithaugustine@gmail.com>
2024-10-04 17:54:54 +0300

    Navigate through screens

--- App.tsx
+++ App.tsx
@@ -1,6 +1,30 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import LoginScreen from "./app/screens/LoginScreen";
 import RegisterScreen from "./app/screens/RegisterScreen";
 
 export default function App() {
-  return <RegisterScreen />;
+  const Stack = createNativeStackNavigator();
+
+  const StackNavigator = () => (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="login"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="register"
+        component={RegisterScreen}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
